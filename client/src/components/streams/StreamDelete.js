db62751b660d164646e5f48e60dc1a0de0d39da3
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 18:18:46 -0400

    scaffold delete with modal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Modal from "../Modal";
+
+import history from "../../history";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const onDismiss = () => {
+    history.push("/");
+  };
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button" onClick={onDismiss}>
+        Cancel
+      </button>
+    </>
+  );
+  return (
+    <div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={onDismiss}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
