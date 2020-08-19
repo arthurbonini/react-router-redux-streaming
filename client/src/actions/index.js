b5db4f5eaf11968606d0dd4a2a55259c8a7c7fec
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 16:57:42 -0400

    add redirects to edit and delte

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -36,8 +36,12 @@ export const fetchStream = (id) => async (dispatch) => {
 export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: id });
+
+  history.push("/");
 };
 export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
+
+  history.push("/");
 };
