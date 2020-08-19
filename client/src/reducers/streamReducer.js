fa0fe10c0a49bf6dc608738a408ee41ed6d81ef4
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 15:02:36 -0400

    fix type in streamreducer

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -11,7 +11,7 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(state, "id") };
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
     case CREATE_STREAM:
     case EDIT_STREAM:
