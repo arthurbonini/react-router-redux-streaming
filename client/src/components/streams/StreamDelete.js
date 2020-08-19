9e85731a320194c72d6b3b3a038ebc5625791d1e
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 18:35:10 -0400

    wire up delete stream to actions

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,30 +1,55 @@
 import React from "react";
 import Modal from "../Modal";
+import { connect } from "react-redux";
 
 import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-  const onDismiss = () => {
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  onDismissClick = () => {
     history.push("/");
   };
-  const actions = (
+
+  onDeleteClick = () => {
+    this.props.deleteStream(this.props.stream.id);
+  };
+  renderActions = () => (
     <>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button" onClick={onDismiss}>
+      <button className="ui button negative" onClick={this.onDeleteClick}>
+        Delete
+      </button>
+      <button className="ui button" onClick={this.onDismissClick}>
         Cancel
       </button>
     </>
   );
-  return (
-    <div>
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream?";
+    }
+    return `Are you sure you want to delete this stream: ${this.props.stream.title}`;
+  }
+  render() {
+    return (
       <Modal
         title="Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={onDismiss}
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={this.onDismissClick}
       />
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default StreamDelete;
+const mapStateToProps = (state, { match }) => {
+  return {
+    stream: state.streams[match.params.id],
+  };
+};
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
