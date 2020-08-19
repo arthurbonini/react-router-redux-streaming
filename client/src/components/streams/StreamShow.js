4d742fccb22446839744a8203aa59903e6f310a0
Arthur Bonini <arthur.bonini@prs-invivo.com>
2020-08-19 19:09:29 -0400

    refactor streamshow to fetch show

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { fetchStream } from "../../actions";
+
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h3>Hello Stream</h3>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, { match }) => {
+  return {
+    stream: state.streams[match.params.id],
+  };
 };
-
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
